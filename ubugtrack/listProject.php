1f51995f5ba89f8046f454a4082055bc2a9d235d
naej56 <naej56@hotmail.fr>
2016-08-24 14:19:12 +0200

    test ubugtrack api
    test pour l'utilisation de ubugtrack api

--- ubugtrack/listProject.php
+++ ubugtrack/listProject.php
@@ -1,15 +1,27 @@
 <?php
-	$url = 'https://ubugtrack.com/api/32ae239c155b6075eb02d40db94da886a8e25cb00a5b8b8f3f2700d1bd139858/projects/';
+function listProject($apiUserKey){
+	$url = 'https://ubugtrack.com/api/' . $apiUserKey . '/projects/';
 		
 	$ch = curl_init(); 
 	curl_setopt($ch, CURLOPT_URL, $url); 
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
 	$response = curl_exec($ch);
+
+	if($errno = curl_errno($ch)) {
+    	$error_message = curl_strerror($errno);
+    	echo "cURL error ({$errno}):\n {$error_message}";
+	}
+
 	$result = json_decode($response);
 	var_dump($response);
+	var_dump($result);
 	if ($result->success)
 	{
 		foreach ($result->projects as $project)
 			echo $project->id.' - '.$project->name;
 	}
+}
+
+listProject('e53309a2400c7611377458bdc88e7c755bfd7cf391aa0981f11d143b575bec3d'); //j'ai remplacé içi ma clé par des xxxx
 ?>
